89acf5eefe6a915d4acd54fc8c06f272aca793de
LogicAL007 <ayomidemtsh@gmail.com>
2023-10-17 09:29:28 +0100

    Corrections
     I made corrections to the issues addressed ;
    -don't commit a credential file
    -add docstring to explain what your code does
    -A readme markdown file to give detail insight to your project

--- my_gspread.py
+++ my_gspread.py
@@ -1,13 +1,26 @@
 import gspread
 import pandas as pd
+from dotenv_vault import load_dotenv
+import os 
 
-# create a connection with the service account
-gc = gspread.service_account("omotosho-project-c92e4f4bc071.json")
+load_dotenv()
+API_key =  os.getenv('Project_key')
+
+gc = gspread.service_account(API_key)
 df = pd.read_csv("fileasa.csv")
 spreadsheet = gc.open("Gspread practice")
-# sh = gc.create('A new spreadsheet')
 
 def get_or_create_worksheet(spreadsheet, Ecom , rows=1, cols=1):
+    '''
+    This function gets a worksheet(Ecom) and if not found, it creates the worksheet
+
+    :param spreadsheet: This is the spreadsheet which the  worksheet is gotten from.
+    :param Ecom: This is the worksheet to get or create
+    :param rows: This is the no of row initially created 
+    :param col: This is the no of column initially created
+    :return: The retrieved or newly created worksheet
+    
+    '''
     try:
         worksheet = spreadsheet.worksheet(Ecom)
     except:
@@ -15,12 +28,17 @@ def get_or_create_worksheet(spreadsheet, Ecom , rows=1, cols=1):
     return worksheet
 
 def populate_worksheet(Ecom):
+    '''
+    This function puts the header of a dataframe into the first row of a sheet followed by the values of a dataframe to the worsksheet that was created or already existing.
+    
+    :param Ecom: The title of the worksheet to populate.
+    '''
     worksheet = get_or_create_worksheet(spreadsheet, Ecom, rows=df.shape[0]+1, cols=df.shape[1])
     worksheet.update([df.columns.values.tolist()] + df.values.tolist())
 
 
 populate_worksheet("Ecommerce store")
+
+#Prints the worksheet in the spreadsheet to check if the worksheet("Ecommerce store") has been created
 worksheet_list = spreadsheet.worksheets()
 print(worksheet_list)
-# worksheet= spreadsheet.worksheet("Ecommerce store")
-# print(worksheet)
