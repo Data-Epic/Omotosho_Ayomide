7313be7dced53068f981f720e71f8c5e121879ee
LogicAL007 <ayomidemtsh@gmail.com>
2023-10-18 12:24:09 +0100

    A comment was added to explain the df.shape[0]+1 and the parameter were updated
    - change title format for parameters "Ecom" should be "ecom"
    
    -comment on this line to help us know why you are doing df.shape[0]+1.

--- my_gspread.py
+++ my_gspread.py
@@ -18,32 +18,32 @@ spreadsheet = gc.open("Gspread practice")
 
 logging.basicConfig(level=logging.INFO)
 
-def get_or_create_worksheet(spreadsheet, Ecom , rows=1, cols=1):
+def get_or_create_worksheet(spreadsheet, ecom , rows=1, cols=1):
     '''
     This function gets a worksheet(Ecom) and if not found, it creates the worksheet
 
-    :param spreadsheet: This is the spreadsheet which the  worksheet is gotten from.
-    :param Ecom: This is the worksheet to get or create
+    :param spreadsheet: This is the spreadsheet which the worksheet is gotten from.
+    :param ecom: This is the worksheet to get or create
     :param rows: This is the no of row initially created 
     :param col: This is the no of column initially created
     :return: The retrieved or newly created worksheet
-    
     '''
     try:
-        worksheet = spreadsheet.worksheet(Ecom)
+        worksheet = spreadsheet.worksheet(ecom)
         logging.info("This worksheet already exist")
     except:
-        worksheet = spreadsheet.add_worksheet(Ecom, rows, cols)
+        worksheet = spreadsheet.add_worksheet(ecom, rows, cols)
         logging.info("The worksheet was not found, a new one will be created")
     return worksheet
 
-def populate_worksheet(Ecom):
+def populate_worksheet(ecom):
     '''
     This function puts the header of a dataframe into the first row of a sheet followed by the values of a dataframe to the worsksheet that was created or already existing.
     
-    :param Ecom: The title of the worksheet to populate.
+    :param ecom: The title of the worksheet to populate.
     '''
-    worksheet = get_or_create_worksheet(spreadsheet, Ecom, rows=df.shape[0]+1, cols=df.shape[1])
+    # this worksheet is created based on the number of rows and column in the df
+    worksheet = get_or_create_worksheet(spreadsheet, ecom, rows=df.shape[0]+1, cols=df.shape[1])
     worksheet.update([df.columns.values.tolist()] + df.values.tolist())
     logging.info("The data has been successfully loaded into the spreadsheet")
 
